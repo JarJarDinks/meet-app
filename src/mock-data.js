4f428efb57c8996b158fea72077415c84db10371
JarJarDinks <Cochranejacob22@gmail.com>
2023-06-13 17:24:52 -0700

    add mock data for tests

---
+++ src/mock-data.js
@@ -0,0 +1,12 @@
+const mockData = [
+  {
+    kind: 'calendar#events',
+    etag: '"p32k8956movffs0g"',
+  },
+  {
+    kind: 'calendar#event',
+    etag: '"3181161784712000"',
+  },
+];
+
+export default mockData;
